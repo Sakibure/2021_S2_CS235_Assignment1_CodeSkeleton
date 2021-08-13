e77357e6ab2ff53586abc56fadf959aada36c6a4
Sakibure <victor526040181@gmail.com>
2021-08-13 13:51:50 +1200

    update 3

--- domainmodel/book.py
+++ domainmodel/book.py
@@ -1,15 +1,16 @@
+from domainmodel.author import Author
 from domainmodel.publisher import Publisher
 
 
 class Book:
-    def __init__(self, id, title):
+    def __init__(self, book_id, title):
         self.__publisher = None
         self.__description = None
         self.__authors = []
         self.__publish_year = None
         self.__ebook = False
-        if isinstance(id, int):
-            self.__book_id = id
+        if isinstance(book_id, int):
+            self.__book_id = book_id
         else:
             raise ValueError
         if isinstance(title, str) and len(title.strip()) > 0:
@@ -22,8 +23,8 @@ class Book:
         return self.__publisher
 
     @publisher.setter
-    def publisher(self, publisher):
-        self.__publisher = publisher
+    def publisher(self, pub):
+        self.__publisher = pub
 
     @property
     def authors(self):
@@ -37,8 +38,27 @@ class Book:
         return f"<Book {self.__book_title}, book id = {self.__book_id}>"
 
     def __eq__(self, other):
-        pass
+        if not isinstance(other, self.__class__):
+            return False
+        return self.__book_id < other.__book_id
 
     def __hash__(self):
-        pass
-    def add_author(author):
+        return hash(self.__book_id)
+
+    def add_author(self, au):
+        if au not in self.__authors:
+            self.__authors.append(au)
+            au.__authors.append(self)
+
+
+if __name__ == '__main__':
+    book = Book(84765876, "Harry Potter")
+    print(book)
+
+    publisher = Publisher("Bloomsbury")
+    book.publisher = publisher
+    print(book.publisher)
+
+    author = Author(635, "J.K. Rowling")
+    book.add_author(author)
+    print(book.authors)
