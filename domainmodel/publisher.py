1d563cf5e00626ba0871721f1b45f5c516f71990
Sakibure <victor526040181@gmail.com>
2021-08-13 12:53:37 +1200

    update 2

--- domainmodel/publisher.py
+++ domainmodel/publisher.py
@@ -1,8 +1,7 @@
 class Publisher:
 
     def __init__(self, publisher_name: str):
-        self.__name = None
-        if isinstance(publisher_name,str) is False or len(publisher_name.strip()) is 0:
+        if isinstance(publisher_name, str) is False or len(publisher_name.strip()) == 0:
             self.__name = "N/A"
         self.__publisher = publisher_name.strip()
 
@@ -16,15 +15,26 @@ class Publisher:
 
     def __repr__(self):
         # we use access via the property here
-        return f'<Publisher {self.name}>'
+        return f'<Publisher {self.__name}>'
 
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
             return False
-        return other.name == self.name
+        return other.__name == self.__name
 
     def __lt__(self, other):
         return self.__name < other.__name
 
     def __hash__(self):
         return hash(self.__name)
+
+
+if __name__ == '__main__':
+    publisher1 = Publisher("Avatar Press")
+    print(publisher1)
+    publisher2 = Publisher("  ")
+    print(publisher2)
+    publisher3 = Publisher("  DC Comics ")
+    print(publisher3)
+    publisher4 = Publisher(42)
+    print(publisher4)
