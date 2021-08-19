35d62d33eed73f31da65a3b7eb8d7cda7642d174
Sakibure <victor526040181@gmail.com>
2021-08-19 15:02:44 +1200

    inventory update

--- domainmodel/inventory.py
+++ domainmodel/inventory.py
@@ -1,12 +1,23 @@
 from domainmodel.book import Book
+from domainmodel.author import Author
+from domainmodel.publisher import Publisher
 
 
 class BooksInventory:
     def __init__(self, price, num_items):
-        self.book = Book.collection()
-        self.price = price
-        self.num_items = num_items
+        self.__book = Book
+        self.__price = price
+        self.__num_items = num_items
 
     def add_book(self, book, price, nr_books_in_stock):
-        wtf is this
+
+    def remove_book(self, book_id):
+
+    def find_book(self, book_id):
+
+    def find_price(self, book_id):
+
+    def find_stock_count(self, book_id):
+
+    def search_book_by_title(self, title):
 
